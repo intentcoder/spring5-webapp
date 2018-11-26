ad36a6b7561027205492e8ab13298a155bef18df
Sunil Bhambani <sunil_bhambani@persistent.co.in>
2018-11-26 18:11:00 +0530

    Added publisher entity

---
+++ src/main/java/com/intentcoder/model/Publisher.java
@@ -0,0 +1,68 @@
+package com.intentcoder.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String address;
+	
+	public Publisher(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+			
+		Publisher publisher = (Publisher) obj;
+		return id != null ? id.equals(publisher.id) : publisher.id == null;
+	}
+
+	@Override
+	public String toString() {
+		return "Publisher [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+}
---
+++ src/main/java/com/intentcoder/repositories/PublisherRepository.java
@@ -0,0 +1,9 @@
+package com.intentcoder.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.intentcoder.model.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
